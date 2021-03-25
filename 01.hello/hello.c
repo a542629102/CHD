47c2de35226889fe13369f73e2cbd646837272da
CHD <542629102@qq.com>
2021-03-25 14:57:28 +0800

    hello

---
+++ 01.hello/hello.c
@@ -0,0 +1,8 @@
+#include<stdlib.h>
+#include"my_print.h"
+int main(void)
+{
+	print_hello();
+	exit(0);
+}
+
---
+++ 01.hello/my_print.c
@@ -0,0 +1,5 @@
+#include<stdio.h>
+void print_hello(void)
+{
+	printf("hello,linux c world2!\n");	
+}
